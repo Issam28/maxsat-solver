44d2c698113764f0e15f65c5bbcb5a0f8f22e0ec
colobas <guilherme.pg.pires@gmail.com>
2016-04-07 18:17:31 +0100

    working omp version. unexplained memory leak (independent of the size of the problem and OMP related.)

--- maxsat-omp.c
+++ maxsat-omp.c
@@ -4,26 +4,29 @@
 #include <omp.h>
 
 
-void MAXSAT(int n_clauses, int n_vars, int** clause_matrix, int cur_var, int* prev_comb);
-int** parseFile(int * n_clauses, int * n_vars, char name_of_the_file[40]);
+void MAXSAT(int cur_var, int* prev_comb, int root);
+int** parseFile(char name_of_the_file[40]);
 int get_bit(int* decimal, int N);
-int* get_cur_comb(int cur_var, int* prev_comb, int n_vars);
-int* get_first_comb(int n_vars);
-int clauses_satisfied(int n_clauses, int** clause_matrix, int cur_var, int* cur_comb, int* n_clauses_unsatisfied);
-void print_sol(int* cur_sol, int n_vars);
-void free_matrix(int **clause_matrix, int n_clauses);
-int on_same_branch(int* comb1, int* comb2, int cur_var);
-int get_last_var_on_comb(int* comb, int n_vars);
-int copy_array(int* dest, int* src, int n_vars);
-int cmp_array(int* comb1, int* comb2, int up_until);
-
+int* get_cur_comb(int cur_var, int* prev_comb);
+int* get_first_comb();
+int clauses_satisfied(int cur_var, int* cur_comb, int* n_clauses_unsatisfied);
+void print_sol();
+void free_matrix();
+void copy_array(int* dest, int* src);
+int are_there_idle_threads();
 
 // TOOD: handle concurrent access to global variables
 int cur_maxsat=0;
 int n_solutions=0;
 int* cur_sol;
-int* last_seen;
+int* thread_status;
+int n_threads;
+int n_clauses;
+int n_vars;
 
+int** clause_matrix;
+
+omp_lock_t print_lock;
 
 // TODO: implement global variables to keep track of the most recent node being processed.
 //		-> If we want to do BFS, then we jump to one of the children of that node
@@ -31,42 +34,55 @@ int* last_seen;
 
 
 
-void main(int argc, char** argv){
+int main(int argc, char** argv){
 
+	n_threads = omp_get_max_threads();
+	omp_init_lock(&print_lock);
 	if(argc!=2){
 		printf("Usage: maxsat-serial input-file.in");
 		exit(1);
 	}
 
+	thread_status = (int *) malloc(n_threads*sizeof(int));
+	for(int i=0; i<n_threads; i++)
+		thread_status[i]=0;
 
-	int n_clauses, n_vars;
-	int **clause_matrix = parseFile(&n_clauses, &n_vars, argv[1]);
+	clause_matrix = parseFile(argv[1]);
+
 	int *first_comb = get_first_comb(n_vars);
-	last_seen = get_first_comb(n_vars);
-
-	#pragma omp parallel
-	{		
-		#pragma omp single
-		{
-			MAXSAT(n_clauses, n_vars, clause_matrix,  1, first_comb); //branch with first variable set to true
-		}			
-		#pragma omp single
-		//#pragma omp task
-		{
-			MAXSAT(n_clauses, n_vars, clause_matrix, -1, first_comb); //branch with first bariable set to false
-		}
-		//#pragma taskwait
-		}
-	//}
-
+	first_comb[0]=1;
+	int *first_comb2 = get_first_comb(n_vars);
+	first_comb2[0]=-1;
+	cur_sol = get_first_comb(n_vars);
 	
 
+//	printf("\n%d threads\n", n_threads);
+	#pragma omp parallel
+	{
+		#pragma omp single nowait
+		{
+			if(are_there_idle_threads()){
+				#pragma omp task
+				{
+					MAXSAT(-1, first_comb2, 1); //branch with first bariable set to false
+				}
+			}
+			else{
+				MAXSAT(-1, first_comb2, 1);
+			}
+
+			MAXSAT(1, first_comb, 1); //branch with first variable set to true
+		}
+	}
+
+	free(thread_status);
+	omp_destroy_lock(&print_lock);
 
 	printf("%d %d\n", cur_maxsat, n_solutions);
 	print_sol(cur_sol, n_vars);
 
 
-	free_matrix(clause_matrix, n_clauses);
+	free_matrix();
 	free(cur_sol);
 	exit(0);
 }
@@ -75,112 +91,82 @@ void main(int argc, char** argv){
 
 
 //TODO: Learn how to associate branches with threads, to make sure the same branch isn't processed twice, for instance when an idle thread takes work previously associated with a busy thread
-void MAXSAT(int n_clauses, int n_vars, int** clause_matrix, int cur_var, int* prev_comb){
+void MAXSAT(int cur_var, int* cur_comb, int root){
 	int n_clauses_satisfied;
 	int n_clauses_unsatisfied=0;
 	int next_var;
-	int to_free=0;
-	int out=0;
-	int * prev_comb2;
-	int n;
-	int* cur_comb = get_cur_comb(cur_var, prev_comb, n_vars); // knowing the current variable assignment and the previous combination,
-													          // get the current combination
+	int tid = omp_get_thread_num();
+	int* next_comb;
+	int* next_comb2;
+	
+	n_clauses_satisfied = clauses_satisfied(cur_var, cur_comb, &n_clauses_unsatisfied); //calculate number of clauses satisfied and unsatisfied by current combination
+
+	// omp_set_lock(&print_lock);
+	// printf("\n [%lf] thd(%d) -> var: %d || clauses_satisfied: %d || cur_maxsat: %d || n_solutions: %d || comb: ", omp_get_wtime(), tid, cur_var, n_clauses_satisfied, cur_maxsat, n_solutions); fflush(stdout);
+	// print_array(cur_comb, n_vars); omp_unset_lock(&print_lock);
 
 
-	if(cur_var==0){
-		cur_var = - get_last_var_on_comb(prev_comb, n_vars);
-	}
-
-	#pragma omp critical
-	{
-		n = get_last_var_on_comb(last_seen,n_vars);
-
-		if(abs(n) < abs(cur_var) ){
-			copy_array(last_seen, cur_comb, n_vars); //this is the last seen
-			printf("\nupdated last seen. %d %d\n", n, cur_var); fflush(stdout);
-		}else{
-			if(cmp_array(last_seen, cur_comb, abs(cur_var))){ //node is already being processed -> call sibling of last_seen
-				prev_comb2 = malloc(n_vars*sizeof(int));
-				copy_array(prev_comb2, last_seen, n_vars);
-				
-				prev_comb2[abs(cur_var)]=0;
-				out=1;
-			}
-		}
-	}
-
-//DEAD FUCKING LOCK
-	if(out){
-		printf("Fuck you %d.\n", cur_var);
-		MAXSAT(n_clauses, n_vars, clause_matrix, -cur_var, prev_comb2);
-		return;
-	}
-	//printf("%d (%d)\n", cur_var, omp_get_thread_num() ); fflush(stdout);
-
-	n_clauses_satisfied = clauses_satisfied(n_clauses, clause_matrix, cur_var, cur_comb, &n_clauses_unsatisfied); //calculate number of clauses satisfied and unsatisfied by current combination
 	if(abs(cur_var)<n_vars){ //we're not on the last variable -> we're not on a leaf
-
-
-		//If the condition isn't met, there's no need to go further on this, no better solution we'll be found -> suggestion from the project sheet	
-		// TODO: In that case this thread is available to process other branch -> take it to the correct branch.
+		//prune
 		if(n_clauses - n_clauses_unsatisfied < cur_maxsat){ //no need to go further, no better solution we'll be found -> suggestion from the project sheet
-			free(cur_comb);
-			int* prev_comb2 = malloc(n_vars*sizeof(int));
-			
-			copy_array(prev_comb2, last_seen, n_vars);
-				
-			prev_comb2[abs(cur_var)]=0;
+//			printf("\nprune tid:%d cur_var:%d\n",tid,cur_var);fflush(stdout);
+			#pragma atomic write
+					thread_status[tid]=0;
 
-			MAXSAT(n_clauses, n_vars, clause_matrix, 0, prev_comb2);
+			free(cur_comb);
+			return;
 		}
 		else{
 			//continue going down the tree
 			next_var = abs(cur_var)+1;
-			
-			// TODO: Some logic here to understand if the children are already being processed by some other thread.
-			//		 If so, this thread is available to process other branch -> take it to the correct branch.
-			//
-		
-				MAXSAT(n_clauses, n_vars, clause_matrix,  next_var, cur_comb); //branch with next var set to true
-			
-			//#pragma omp single
-			#pragma omp task 
-			{
-				MAXSAT(n_clauses, n_vars, clause_matrix, -next_var, cur_comb); //branch with next var set to false
+			next_comb = get_cur_comb(-next_var, cur_comb);
+			next_comb2 = get_cur_comb(next_var, cur_comb);
+
+			if(are_there_idle_threads()){
+				#pragma omp task 
+				{
+				MAXSAT(-next_var, next_comb, 1); //branch with next var set to false
+				}
 			}
-		
-	//		#pragma taskwait
+			else
+				MAXSAT(-next_var, next_comb, 0);
 			
+
+			MAXSAT(next_var, next_comb2, 0); //branch with next var set to true
+
+						
+			if(root){
+				#pragma atomic write
+					thread_status[tid]=0;
+			}
+
+			free(cur_comb);
+			return;
 		}
-		return;
 	}
 	else{ //we're in a leaf
-		#pragma omp critical
+		#pragma omp critical (solutions)
 		{
 			if(n_clauses_satisfied == cur_maxsat){ //if this combination satisfies the same number of clauses as the current best, increase the number of solutions
 				n_solutions++;
-				print_sol(cur_comb,n_vars);
-				printf("%d (%d)\n\n", n_solutions, omp_get_thread_num() ); fflush(stdout);
-				free(cur_comb);
 			}
 			else{
 				if(n_clauses_satisfied > cur_maxsat){ //if this combination satisfies more clauses than the previous best...
-					cur_sol = cur_comb; //store the solution
+					copy_array(cur_sol,cur_comb); //store the solution
 					cur_maxsat = n_clauses_satisfied; //update the best score
 					n_solutions=1;
-					print_sol(cur_comb,n_vars);
-					printf("%d (%d)\n\n", n_solutions, omp_get_thread_num() ); fflush(stdout);
 				}
-				else
-					to_free=1;
 			}
 		}
-		if(to_free)
-			free(cur_comb);
-
+		
+		if(root){
+			#pragma atomic write
+				thread_status[tid]=0;
+		}
+		free(cur_comb);
 		return;
-	}
 
+	}
 
 }
 
@@ -220,25 +206,20 @@ void MAXSAT(int n_clauses, int n_vars, int** clause_matrix, int cur_var, int* pr
 
 // Utility functions down there
 
-int get_last_var_on_comb(int* comb, int n_vars){
-	for(int i=0; i<n_vars; i++)
-		if(comb[i]==0)
-			return i;
+
+int are_there_idle_threads(){
+	
+	for(int i=0; i<n_threads; i++)
+		if(thread_status[i]==0)
+			return 1;
+
+	return 0;	
 }
 
-int cmp_array(int* comb1, int* comb2, int up_until){
-	
-	for(int i=0; i<up_until; i++)
-		if(comb1[i]!=comb2[i])
-			return 0;
-	
-	return 1;
-}
-
-int copy_array(int* dest, int* src, int n_vars){
+void copy_array(int* dest, int* src){
 	for(int i=0; i<n_vars; i++){
 		if(src[i]==0)
-			break;
+		 	break;				
 
 		dest[i]=src[i];
 	}
@@ -246,7 +227,7 @@ int copy_array(int* dest, int* src, int n_vars){
 
 
 
-int* get_first_comb(int n_vars){
+int* get_first_comb(){
 	int* first_comb = (int *) malloc(n_vars*sizeof(int));
 	int i;
 
@@ -258,17 +239,12 @@ int* get_first_comb(int n_vars){
 
 
 //to obtain the combination, we copy the previous combination and assign the current variable accordingly. actually we only copy half of the time. half of the children reuse the parent's array, the other's copy it.
-int* get_cur_comb(int cur_var, int* prev_comb, int n_vars){
+int* get_cur_comb(int cur_var, int* prev_comb){
 	int* cur_comb;
-	int i;
-
-	if(cur_var<0)
-		cur_comb = prev_comb;
-	else{
-		cur_comb = (int *) malloc(n_vars*sizeof(int));
-		for(i=0; i<n_vars; i++)
-			cur_comb[i]=prev_comb[i];
-	}
+	
+	cur_comb = (int *) malloc(n_vars*sizeof(int));
+	for(int i=0; i<n_vars; i++)
+		cur_comb[i]=prev_comb[i];
 
 	cur_comb[abs(cur_var)-1]=cur_var;
 
@@ -276,12 +252,12 @@ int* get_cur_comb(int cur_var, int* prev_comb, int n_vars){
 }
 
 //for a given combination and current variable, calculate the number of clauses satisfied (and unsatisfied)
-int clauses_satisfied(int n_clauses, int** clause_matrix, int cur_var, int* cur_comb, int* unsatisfied){
+int clauses_satisfied(int cur_var, int* cur_comb, int* unsatisfied){
 	int i, j, n_clauses_satisfied=0;
 	int unsat = 0;
-	int var, bit;
+	int var;
 
-#pragma omp parallel for
+//#pragma omp parallel for
 	for(i=0; i<n_clauses; i++){ //for each clause
 		for(j=0; j<20; j++){ //for each variable -> each variable is a bit of cur_ass
 			var = abs(clause_matrix[i][j]);
@@ -290,7 +266,7 @@ int clauses_satisfied(int n_clauses, int** clause_matrix, int cur_var, int* cur_
 				break;
 			
 			if(cur_comb[var-1] == clause_matrix[i][j]){ //if the variable corresponds
-				#pragma omp atomic
+//				#pragma omp atomic
 					n_clauses_satisfied++;
 				
 				unsat=0;
@@ -301,7 +277,7 @@ int clauses_satisfied(int n_clauses, int** clause_matrix, int cur_var, int* cur_
 		}
 
 		if(unsat==1 && clause_matrix[i][j]==0){
-			#pragma omp atomic write
+//			#pragma omp atomic write
 				*unsatisfied = *unsatisfied + 1;
 			
 			unsat=0;
@@ -311,7 +287,7 @@ int clauses_satisfied(int n_clauses, int** clause_matrix, int cur_var, int* cur_
 	return n_clauses_satisfied;
 }
 
-void print_sol(int* cur_sol, int n_vars){
+void print_sol(){
 	int i;
 	for(i=0; i<n_vars; i++){
 		printf("%d ", cur_sol[i]);
@@ -321,7 +297,7 @@ void print_sol(int* cur_sol, int n_vars){
 	printf("\n");
 }
 
-void free_matrix(int** clause_matrix, int n_clauses){
+void free_matrix(){
 	int i;
 	for(i=0; i<n_clauses; i++)
 		free(clause_matrix[i]);
@@ -329,9 +305,8 @@ void free_matrix(int** clause_matrix, int n_clauses){
 	free(clause_matrix);
 }
 
-int **parseFile(int * n_clauses, int * n_vars, char name_of_the_file[40]){
+int **parseFile(char name_of_the_file[40]){
 	int n,j,i = 0;
-   	long elapsed_seconds;
   	char line[80];
    	int matrix_line = 0, matrix_column = 0;
    	int field = -1;
@@ -342,18 +317,18 @@ int **parseFile(int * n_clauses, int * n_vars, char name_of_the_file[40]){
    	
    	/*Read n_vars and n_clauses*/
 	fgets(line,80,fr);
-	sscanf(line, "%d" "%d", n_vars, n_clauses);
+	sscanf(line, "%d" "%d", &n_vars, &n_clauses);
 	//printf("N_vars =  %d  N_clauses = %d \n",*n_vars,*n_clauses);
 
 
 
-    clause_matrix = (int**)malloc( *n_clauses * sizeof( int* ));
+    clause_matrix = (int**)malloc( n_clauses * sizeof( int* ));
 
-    for (i = 0; i < *n_clauses; i++){
+    for (i = 0; i < n_clauses; i++){
     	clause_matrix[i] = (int *)malloc(20 * sizeof(int));
  	}
 
- 	for (i = 0; i < *n_clauses; i++){
+ 	for (i = 0; i < n_clauses; i++){
  		for (j = 0; j < 20; j++)
  		{
  			clause_matrix[i][j] = 0;
