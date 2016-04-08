3c2202011336c1d06b4709203ca67376583f94c0
colobas <guilherme.pg.pires@gmail.com>
2016-04-08 14:59:08 +0100

    final version

--- maxsat-omp.c
+++ maxsat-omp.c
@@ -15,29 +15,20 @@ void free_matrix();
 void copy_array(int* dest, int* src);
 int are_there_idle_threads();
 
-// TOOD: handle concurrent access to global variables
-int cur_maxsat=0;
-int n_solutions=0;
-int* cur_sol;
-int* thread_status;
-int n_threads;
-int n_clauses;
-int n_vars;
 
-int** clause_matrix;
-
-omp_lock_t print_lock;
-
-// TODO: implement global variables to keep track of the most recent node being processed.
-//		-> If we want to do BFS, then we jump to one of the children of that node
-//		-> If we want to do DFS, then we jump to the brother of that node
-
-
+int cur_maxsat=0; 			//contains current best score
+int n_solutions=0; 			//contains number of solutions that reach the best score
+int* cur_sol; 				//contains one combination that reaches the best score
+int* thread_status;			//vector of thread status. each thread can be idle(0) or active(1)
+int n_threads;				//number of threads
+int n_clauses;				//number of clauses
+int n_vars;					//number of variables
+int** clause_matrix;		//matrix with all the clauses to be tested
 
 int main(int argc, char** argv){
 
 	n_threads = omp_get_max_threads();
-	omp_init_lock(&print_lock);
+	
 	if(argc!=2){
 		printf("Usage: maxsat-serial input-file.in");
 		exit(1);
@@ -49,19 +40,24 @@ int main(int argc, char** argv){
 
 	clause_matrix = parseFile(argv[1]);
 
+	//allocate needed arrays
 	int *first_comb = get_first_comb(n_vars);
 	first_comb[0]=1;
+	
 	int *first_comb2 = get_first_comb(n_vars);
 	first_comb2[0]=-1;
+	
 	cur_sol = get_first_comb(n_vars);
 	
 
-//	printf("\n%d threads\n", n_threads);
+	//root node of the binary tree.
 	#pragma omp parallel
 	{
 		#pragma omp single nowait
 		{
-			if(are_there_idle_threads()){
+			//here the two children nodes are spawned. if there are avaiable threads, a task is created. 
+			//otherwise, this thread will process both nodes
+			if(are_there_idle_threads()){ 
 				#pragma omp task
 				{
 					MAXSAT(-1, first_comb2, 1); //branch with first bariable set to false
@@ -76,7 +72,7 @@ int main(int argc, char** argv){
 	}
 
 	free(thread_status);
-	omp_destroy_lock(&print_lock);
+	
 
 	printf("%d %d\n", cur_maxsat, n_solutions);
 	print_sol(cur_sol, n_vars);
@@ -87,10 +83,6 @@ int main(int argc, char** argv){
 	exit(0);
 }
 
-
-
-
-//TODO: Learn how to associate branches with threads, to make sure the same branch isn't processed twice, for instance when an idle thread takes work previously associated with a busy thread
 void MAXSAT(int cur_var, int* cur_comb, int root){
 	int n_clauses_satisfied;
 	int n_clauses_unsatisfied=0;
@@ -101,15 +93,11 @@ void MAXSAT(int cur_var, int* cur_comb, int root){
 	
 	n_clauses_satisfied = clauses_satisfied(cur_var, cur_comb, &n_clauses_unsatisfied); //calculate number of clauses satisfied and unsatisfied by current combination
 
-	// omp_set_lock(&print_lock);
-	// printf("\n [%lf] thd(%d) -> var: %d || clauses_satisfied: %d || cur_maxsat: %d || n_solutions: %d || comb: ", omp_get_wtime(), tid, cur_var, n_clauses_satisfied, cur_maxsat, n_solutions); fflush(stdout);
-	// print_array(cur_comb, n_vars); omp_unset_lock(&print_lock);
-
-
 	if(abs(cur_var)<n_vars){ //we're not on the last variable -> we're not on a leaf
-		//prune
-		if(n_clauses - n_clauses_unsatisfied < cur_maxsat){ //no need to go further, no better solution we'll be found -> suggestion from the project sheet
-//			printf("\nprune tid:%d cur_var:%d\n",tid,cur_var);fflush(stdout);
+
+		//prune condition
+		if(n_clauses - n_clauses_unsatisfied < cur_maxsat){
+		//no need to go further, no better solution we'll be found -> suggestion from the project sheet
 			#pragma atomic write
 					thread_status[tid]=0;
 
@@ -122,20 +110,24 @@ void MAXSAT(int cur_var, int* cur_comb, int root){
 			next_comb = get_cur_comb(-next_var, cur_comb);
 			next_comb2 = get_cur_comb(next_var, cur_comb);
 
+
+			//here the two children nodes are spawned. if there are avaiable threads, a task is created. 
+			//otherwise, this thread will process both nodes
 			if(are_there_idle_threads()){
 				#pragma omp task 
 				{
-				MAXSAT(-next_var, next_comb, 1); //branch with next var set to false
+				MAXSAT(-next_var, next_comb, 1); //branch with next var set to false and "root" set to true.
+												 //this means the thread will return up until here and then become idle again.
 				}
 			}
 			else
-				MAXSAT(-next_var, next_comb, 0);
+				MAXSAT(-next_var, next_comb, 0); //branch with next var set to false
 			
 
 			MAXSAT(next_var, next_comb2, 0); //branch with next var set to true
 
 						
-			if(root){
+			if(root){ //if this is a root node, the thread becomes idle here. otherwise, it still has to return until its root node
 				#pragma atomic write
 					thread_status[tid]=0;
 			}
@@ -159,15 +151,13 @@ void MAXSAT(int cur_var, int* cur_comb, int root){
 			}
 		}
 		
-		if(root){
+		if(root){ //if this is a root node, the thread becomes idle here. otherwise, it still has to return until its root node
 			#pragma atomic write
 				thread_status[tid]=0;
 		}
 		free(cur_comb);
 		return;
-
 	}
-
 }
 
 
@@ -243,8 +233,8 @@ int* get_cur_comb(int cur_var, int* prev_comb){
 	int* cur_comb;
 	
 	cur_comb = (int *) malloc(n_vars*sizeof(int));
-	for(int i=0; i<n_vars; i++)
-		cur_comb[i]=prev_comb[i];
+	
+	copy_array(cur_comb, prev_comb);
 
 	cur_comb[abs(cur_var)-1]=cur_var;
 
@@ -257,7 +247,7 @@ int clauses_satisfied(int cur_var, int* cur_comb, int* unsatisfied){
 	int unsat = 0;
 	int var;
 
-//#pragma omp parallel for
+
 	for(i=0; i<n_clauses; i++){ //for each clause
 		for(j=0; j<20; j++){ //for each variable -> each variable is a bit of cur_ass
 			var = abs(clause_matrix[i][j]);
@@ -266,9 +256,7 @@ int clauses_satisfied(int cur_var, int* cur_comb, int* unsatisfied){
 				break;
 			
 			if(cur_comb[var-1] == clause_matrix[i][j]){ //if the variable corresponds
-//				#pragma omp atomic
-					n_clauses_satisfied++;
-				
+				n_clauses_satisfied++;		
 				unsat=0;
 				break; //only one variable needs to match for the clause to be satisfied
 			}
@@ -277,13 +265,10 @@ int clauses_satisfied(int cur_var, int* cur_comb, int* unsatisfied){
 		}
 
 		if(unsat==1 && clause_matrix[i][j]==0){
-//			#pragma omp atomic write
-				*unsatisfied = *unsatisfied + 1;
-			
+			*unsatisfied = *unsatisfied + 1;
 			unsat=0;
 		}
 	}
-	
 	return n_clauses_satisfied;
 }
 
