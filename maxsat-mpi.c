ef12a9296b672219cbfbae41b57f68bfdd7ae56a
colobas <guilherme.pg.pires@gmail.com>
2016-05-10 01:14:32 +0100

    last commit. results tested for every input file

--- maxsat-mpi.c
+++ maxsat-mpi.c
@@ -22,7 +22,7 @@ int* get_cur_sat_clauses(int cur_var, int* prev_sat_clauses, int n_clauses);
 
 int main(int argc, char** argv ){
     int rank, size;
-    double t1,t2;
+    /*double t1,t2;*/
     MPI_Comm new_comm;
 
     if(argc!=2){
@@ -34,7 +34,7 @@ int main(int argc, char** argv ){
     MPI_Comm_rank( MPI_COMM_WORLD, &rank );
 
     MPI_Barrier(MPI_COMM_WORLD);
-    t1 = MPI_Wtime();
+/*    t1 = MPI_Wtime();*/
     if (rank == 0){
 			masterProc(argc, argv );
     }
@@ -42,13 +42,13 @@ int main(int argc, char** argv ){
 			slaveProc();
     }
     MPI_Barrier(MPI_COMM_WORLD);
-    t2=MPI_Wtime();
+/*  t2=MPI_Wtime();*/
     MPI_Finalize( );
 
 
-    if(rank==0)
+/*  if(rank==0)
       printf("Time elapsed: %f\n", t2-t1);
-
+*/
     return 0;
 }
 
@@ -67,7 +67,7 @@ int masterProc(int argc, char** argv ){
     int* cur_sol;
     int* final_sol;
     int* cur_comb;
-    int idle = 0; //number of processors to which the master didn't send work yet
+    int idle = 0; /*number of processors to which the master didn't send work yet*/
     int depth;
     int cur_var;
     int to_see;
@@ -83,38 +83,44 @@ int masterProc(int argc, char** argv ){
     MPI_Comm_size( MPI_COMM_WORLD, &size );
 
     idle=size-1;
-//    printf("Idle: %d\n", idle);
+/*    printf("Idle: %d\n", idle*/
     depth = (int) floor(log2(size));
-//    printf("Depth: %d\n", depth);
+/*    printf("Depth: %d\n", depth*/
     to_see = pow(2,depth);
-//    printf("To_see: %d\n", to_see);
+/*    printf("To_see: %d\n", to_see*/
 
     cur_comb = distributeNodes(depth, 0, first_comb, n_vars, &idle, &to_see, &cur_var);
     if(cur_comb!=NULL){
       cur_sol = MAXSAT(n_clauses, n_vars, clause_matrix, abs(cur_var)+1, cur_comb, cur_sol, &cur_maxsat, &n_solutions, first_sat_clauses);
-      cur_sol = MAXSAT(n_clauses, n_vars, clause_matrix, -abs(cur_var)-1, cur_comb, cur_sol, &cur_maxsat, &n_solutions, first_sat_clauses);
+
+      if(abs(cur_var)<n_vars){
+        cur_sol = MAXSAT(n_clauses, n_vars, clause_matrix, -abs(cur_var)-1, cur_comb, cur_sol, &cur_maxsat, &n_solutions, first_sat_clauses);
+      }
     }
 
+  /*  printf("\nMaster found %d solutions: %d\n", n_solutions, cur_maxsat);*/
 
     for(int i=1; i<size; i++){
         MPI_Recv(&temp_maxsat, 1, MPI_INT, i, 0, MPI_COMM_WORLD, &status);
         if(temp_maxsat==cur_maxsat){
-          code=0; //signal slave to send only the number of solutions
+          code=0; /*signal slave to send only the number of solutions*/
           MPI_Send(&code, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
           MPI_Recv(&temp_n_sols, 1, MPI_INT, i, 0, MPI_COMM_WORLD, &status);
           n_solutions=n_solutions+temp_n_sols;
+/*          printf("\nSlave %d found %d solutions: %d \n", i, temp_n_sols, cur_maxsat);*/
         }else{
           if(temp_maxsat>cur_maxsat){
-//          printf("slave %d has a better solution.\n", i);
-            code=1; //signal slave to send the number of solutions and one example solution
+/*          printf("slave %d has a better solution.\n", i);*/
+            code=1; /*signal slave to send the number of solutions and one example solution*/
             MPI_Send(&code, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
             MPI_Recv(&temp_n_sols, 1, MPI_INT, i, 0, MPI_COMM_WORLD, &status);
             n_solutions = temp_n_sols;
             cur_maxsat = temp_maxsat;
             MPI_Recv(cur_sol, n_vars, MPI_INT, i, 0, MPI_COMM_WORLD, &status);
+/*            printf("\nSlave %d found %d solutions: %d \n", i, temp_n_sols, cur_maxsat);*/
           }
           else{
-            code=-1; //signal slave to terminate and don't send anything more.
+            code=-1; /*signal slave to terminate and don't send anything more*/
             MPI_Send(&code, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
           }
         }
@@ -141,15 +147,17 @@ int slaveProc(){
     int* first_sat_clauses;
 
     MPI_Comm_rank( MPI_COMM_WORLD, &rank );
-    clause_matrix = BCastClauseMat(clause_matrix,&n_vars,&n_clauses); //transmit n_vars, n_clauses and the clause_matrix to every node
+    clause_matrix = BCastClauseMat(clause_matrix,&n_vars,&n_clauses); /*transmit n_vars, n_clauses and the clause_matrix to every node*/
     cur_comb = recvProblemParams(&cur_var, n_vars);
     first_sat_clauses = alloc_int_array(n_clauses);
 
     cur_sol = MAXSAT(n_clauses, n_vars, clause_matrix, abs(cur_var)+1, cur_comb, cur_sol, &cur_maxsat, &n_solutions, first_sat_clauses);
-    cur_sol = MAXSAT(n_clauses, n_vars, clause_matrix, -abs(cur_var)-1, cur_comb, cur_sol, &cur_maxsat, &n_solutions, first_sat_clauses);
+
+    if(abs(cur_var)<n_vars)
+      cur_sol = MAXSAT(n_clauses, n_vars, clause_matrix, -abs(cur_var)-1, cur_comb, cur_sol, &cur_maxsat, &n_solutions, first_sat_clauses);
 
 
-    //send results to master
+    /*send results to master*/
     MPI_Send(&cur_maxsat, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
     MPI_Recv(&code, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
     switch(code){
@@ -164,11 +172,7 @@ int slaveProc(){
         break;
     }
 
-    /*if(rank==1){
-      printf("%d %d\n", cur_maxsat, n_solutions);
-      print_sol(cur_sol, n_vars);
-      printf("\n");
-    }*/
+
 		return 0;
 }
 
@@ -186,7 +190,7 @@ int** BCastClauseMat(int **clause_matrix, int* n_vars, int* n_clauses){
   }
 
 	for(int i=0; i< *n_clauses; i++)
-		MPI_Bcast(clause_matrix[i], *n_vars, MPI_INT, 0, MPI_COMM_WORLD); //master sends the matrix, line by line, slaves receive it
+		MPI_Bcast(clause_matrix[i], *n_vars, MPI_INT, 0, MPI_COMM_WORLD); /*master sends the matrix, line by line, slaves receive it*/
 
   return clause_matrix;
 }
@@ -201,22 +205,22 @@ int* distributeNodes(int depth, int cur_var, int* prev_comb, int n_vars, int* id
       aux = distributeNodes(depth, -abs(cur_var)-1, cur_comb, n_vars, idle, to_see, master_var);
     }
     else{
-      if(*to_see < (*idle+1)){ //if there are more processors to "hang" than nodes on this level, "hang" two processors in each of this node's children
+      if(*to_see < ((*idle)+1)){ /*if there are more processors to "hang" than nodes on this level, "hang" two processors in each of this node's children*/
         cur_comb2=get_cur_comb(abs(cur_var)+1, cur_comb, n_vars);
         cur_comb3=get_cur_comb(-abs(cur_var)-1, cur_comb, n_vars);
         sendProblemParams(cur_comb2, abs(cur_var)+1, n_vars, *idle);
         (*idle)=(*idle)-1;
         if(*idle>0){
-          sendProblemParams(cur_comb2, abs(cur_var)+1, n_vars, *idle);
+          sendProblemParams(cur_comb3, -abs(cur_var)-1, n_vars, *idle);
           (*idle)=(*idle)-1;
         }
         else{
-          *master_var = cur_var;
-          aux = cur_comb;
+          *master_var = -abs(cur_var)-1;
+          aux = cur_comb3;
         }
       }
-      else{ //if there is one node per processor, "hang" one processor in this node
-        if(*idle>0){ //if this node isn't the master
+      else{ /*if there is one node per processor, "hang" one processor in this node*/
+        if(*idle>0){ /*if this node isn't the master*/
           sendProblemParams(cur_comb, cur_var, n_vars, *idle);
           (*idle)=(*idle)-1;
           free(cur_comb);
@@ -233,6 +237,7 @@ int* distributeNodes(int depth, int cur_var, int* prev_comb, int n_vars, int* id
 
 
 void sendProblemParams(int* cur_comb, int cur_var, int n_vars, int proc){
+/*  printf("\nNode %d got cur_comb: ", proc); print_sol(cur_comb, n_vars);*/
   MPI_Send(cur_comb, n_vars, MPI_INT, proc, 0, MPI_COMM_WORLD);
   MPI_Send(&cur_var, 1, MPI_INT, proc, 0, MPI_COMM_WORLD);
 }
@@ -251,35 +256,35 @@ int* MAXSAT(int n_clauses, int n_vars, int** clause_matrix, int cur_var, int* pr
 	int n_clauses_unsatisfied=0;
 	int next_var;
 
-	int* cur_comb = get_cur_comb(cur_var, prev_comb, n_vars); //knowing the current variable assignment and the previous combination,
-													         //get the current combination
+	int* cur_comb = get_cur_comb(cur_var, prev_comb, n_vars); /*knowing the current variable assignment and the previous combination*/
+													         /*get the current combination*/
 
   int* cur_sat_clauses = get_cur_sat_clauses(cur_var, sat_clauses, n_clauses);
 
 
-	n_clauses_satisfied = clauses_satisfied(n_clauses, clause_matrix, cur_var, cur_comb, &n_clauses_unsatisfied, cur_sat_clauses); //calculate number of clauses satisfied and unsatisfied by current combination
-	if(abs(cur_var)<n_vars){ //we're not on the last variable -> we're not on a leaf
+	n_clauses_satisfied = clauses_satisfied(n_clauses, clause_matrix, cur_var, cur_comb, &n_clauses_unsatisfied, cur_sat_clauses); /*calculate number of clauses satisfied and unsatisfied by current combination*/
+	if(abs(cur_var)<n_vars){ /*we're not on the last variable -> we're not on a leaf*/
 
-		if(n_clauses - n_clauses_unsatisfied < *cur_maxsat){ //no need to go further, no better solution we'll be found -> suggestion from the project sheet
+		if(n_clauses - n_clauses_unsatisfied < *cur_maxsat){ /*no need to go further, no better solution we'll be found -> suggestion from the project sheet*/
 			free(cur_comb);
       free(cur_sat_clauses);
 		}
-		else{ //continue going down the tree
+		else{ /*continue going down the tree*/
 			next_var = abs(cur_var)+1;
 
-			cur_sol = MAXSAT(n_clauses, n_vars, clause_matrix,  next_var, cur_comb, cur_sol, cur_maxsat, n_solutions, cur_sat_clauses); //branch with next var set to true
-			cur_sol = MAXSAT(n_clauses, n_vars, clause_matrix, -next_var, cur_comb, cur_sol, cur_maxsat, n_solutions, cur_sat_clauses); //branch with next var set to false
+			cur_sol = MAXSAT(n_clauses, n_vars, clause_matrix,  next_var, cur_comb, cur_sol, cur_maxsat, n_solutions, cur_sat_clauses); /*branch with next var set to true*/
+			cur_sol = MAXSAT(n_clauses, n_vars, clause_matrix, -next_var, cur_comb, cur_sol, cur_maxsat, n_solutions, cur_sat_clauses); /*branch with next var set to false*/
 		}
 		return cur_sol;
 	}
-	else{ //we're in a leaf
+	else{ /*we're in a leaf*/
 
-		if(n_clauses_satisfied == *cur_maxsat) //if this combination satisfies the same number of clauses as the current best, increase the number of solutions
+		if(n_clauses_satisfied == *cur_maxsat) /*if this combination satisfies the same number of clauses as the current best, increase the number of solutions*/
 			(*n_solutions)++;
 		else{
-			if(n_clauses_satisfied > *cur_maxsat){ //if this combination satisfies more clauses than the previous best...
-				cur_sol = cur_comb; //store the solution
-				*cur_maxsat = n_clauses_satisfied; //update the best score
+			if(n_clauses_satisfied > *cur_maxsat){ /*if this combination satisfies more clauses than the previous best*/
+				cur_sol = cur_comb; /*store the solution*/
+				*cur_maxsat = n_clauses_satisfied; /*update the best score*/
 				*n_solutions=1;
 				return cur_sol;
 			}
@@ -314,7 +319,7 @@ int* get_cur_sat_clauses(int cur_var, int* prev_sat_clauses, int n_clauses){
 }
 
 
-//to obtain the combination, we copy the previous combination and assign the current variable accordingly. actually we only copy half of the time. half of the children reuse the parent's array, the other's copy it.
+/*to obtain the combination, we copy the previous combination and assign the current variable accordingly. actually we only copy half of the time. half of the children reuse the parent's array, the other's copy it*/
 int* get_cur_comb(int cur_var, int* prev_comb, int n_vars){
 	int* cur_comb;
 	int i;
@@ -335,13 +340,13 @@ int* get_cur_comb(int cur_var, int* prev_comb, int n_vars){
 	return cur_comb;
 }
 
-//for a given combination and current variable, calculate the number of clauses satisfied (and unsatisfied)
+/*for a given combination and current variable, calculate the number of clauses satisfied (and unsatisfied*/
 int clauses_satisfied(int n_clauses, int** clause_matrix, int cur_var, int* cur_comb, int* unsatisfied, int* sat_clauses){
 	int i, j, n_clauses_satisfied=0;
 	int unsat = 0;
 	int var;
 
-	for(i=0; i<n_clauses; i++){ //for each clause
+	for(i=0; i<n_clauses; i++){ /*for each clause*/
 		if(sat_clauses[i]==-1){
 			*unsatisfied = *unsatisfied + 1;
 			unsat=0;
@@ -353,17 +358,17 @@ int clauses_satisfied(int n_clauses, int** clause_matrix, int cur_var, int* cur_
 			continue;
 		}
 
-		for(j=0; j<20; j++){ //for each variable
+		for(j=0; j<20; j++){ /*for each variable*/
 			var = abs(clause_matrix[i][j]);
 
-			if(var==0 || var>abs(cur_var)) // end of clause or we don't know the next variable assignments
+			if(var==0 || var>abs(cur_var)) /* end of clause or we don't know the next variable assignments*/
 				break;
 
-			if(cur_comb[var-1] == clause_matrix[i][j]){ //if the variable corresponds
+			if(cur_comb[var-1] == clause_matrix[i][j]){ /*if the variable corresponds*/
 				sat_clauses[i]=1;
 				n_clauses_satisfied++;
 				unsat=0;
-				break; //only one variable needs to match for the clause to be satisfied
+				break; /*only one variable needs to match for the clause to be satisfied*/
 			}
 			else
 				unsat=1;
@@ -409,7 +414,7 @@ int **parseFile(int * n_clauses, int * n_vars, char name_of_the_file[40]){
    	/*Read n_vars and n_clauses*/
 	fgets(line,80,fr);
 	sscanf(line, "%d" "%d", n_vars, n_clauses);
-	//printf("N_vars =  %d  N_clauses = %d \n",*n_vars,*n_clauses);
+	/*printf("N_vars =  %d  N_clauses = %d \n",*n_vars,*n_clauses*/
 
 
 
@@ -425,22 +430,22 @@ int **parseFile(int * n_clauses, int * n_vars, char name_of_the_file[40]){
  			clause_matrix[i][j] = 0;
  		}
  	}
- 	//printf("-------clauses---------------\n");
+ 	/*printf("-------clauses---------------\n*/
    	while(fgets(line, 80, fr) != NULL){
    		start = line;
 
    		while (sscanf(start, "%d%n", &field, &n) == 1 && field != 0) {
    			clause_matrix[matrix_line][matrix_column] = field;
-        	//printf("%d ", clause_matrix[matrix_line][matrix_column]);
+        	/*printf("%d ", clause_matrix[matrix_line][matrix_column*/
         	start += n;
         	matrix_column ++;
     	}
 
-    //printf("\n");
+    /*printf("\n*/
     matrix_column= 0;
     matrix_line ++;
    	}
-   	//printf("----------------------------\n" );
+   	/*printf("----------------------------\n*/
 
    	fclose(fr);
 
